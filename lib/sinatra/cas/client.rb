28fa8ea7a64fbc7fe8456f5bbf1affa7adc65ef7
Simon Octal <sioctal@gmail.com>
2013-04-29 13:35:58 -0400

    typo

--- lib/sinatra/cas/client.rb
+++ lib/sinatra/cas/client.rb
@@ -63,7 +63,7 @@ module Sinatra
         app.set :username_session_key, :cas_user
         app.set :session_key, :rack.session
   
-        unless env.include?(options[:session_key]): 
+        unless env.include?(options[:session_key])
           fail "you need to set up a session middleware *before* #{self.class}"
         end
 
